6822a1d5c73e84432acfdf31ba7cd429da6c3aeb
MansoorAfzalDurrani <145850263+MansoorAfzalDurrani@users.noreply.github.com>
2023-10-13 10:28:02 +0100

    Excercise 3 getSquareFunction written

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,6 +1,13 @@
 export function getSquares(nums) {
 	if (nums === undefined) throw new Error('nums is required');
 	// Your code here!
+	console.log(nums);
+	let numsSquareArray=[];
+	for(let index=0; index<nums.length; index++){
+		numsSquareArray[index]=nums[index] * nums[index];
+	}
+	//console.log(numsSquareArray);
+	return numsSquareArray;
 }
 
 export function camelCaseWords(words) {
